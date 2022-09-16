db6d1df47ffe33edb77129ffe4d6e6f14233d0de
Rafael Garcia Ruiz <rafael.garcia@collabora.com>
2022-09-16 18:18:15 +0200

    Implements the main function ls
    Read the name of directories
    
    Signed-off-by: Rafael Garcia Ruiz <rafael.garcia@collabora.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::read_dir;
+use std::io::Error;
+
+fn main() -> Result<(), Error> {
+    for element in read_dir(".")? {
+        let directory = element?;
+        println!("{:?}", directory.path());
+    }
+
+    Ok(())
 }
