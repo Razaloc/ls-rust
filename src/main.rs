44cf0cccd7eb1de921e94293a21c6846765a3381
Rafael Garcia Ruiz <rafael.garcia@collabora.com>
2022-09-16 19:16:51 +0200

    Specify format
    Prints path and specify if is a file or a directory
    
    Signed-off-by: Rafael Garcia Ruiz <rafael.garcia@collabora.com>

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,23 @@ use std::io::Error;
 fn main() -> Result<(), Error> {
     for element in read_dir(".")? {
         let directory = element?;
-        println!("{:?}", directory.path());
+        let dir_type;
+        let metadata = &directory.path().metadata()?;
+
+        if metadata.is_file() {
+            dir_type = "file";
+        } else if metadata.is_dir() {
+            dir_type = "dir";
+        } else {
+            dir_type = "???";
+        }
+
+        println!(
+            "{:?}
+                 {:?}",
+            directory.path(),
+            dir_type
+        );
     }
 
     Ok(())
